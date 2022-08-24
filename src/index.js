90978bd283e2c4350de11b824ae58235d774f085
Luiz Guilherme da Costa <luizguilhermeh@yahoo.com.br>
2022-08-24 12:11:34 -0300

    Index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import  ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
