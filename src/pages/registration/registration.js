3db652d69cc6d28959cdcb8f82d471c9dac38785
Luiz Guilherme da Costa <luizguilhermeh@yahoo.com.br>
2022-08-24 12:14:33 -0300

    Registration.js

---
+++ src/pages/registration/registration.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './registration.css'
+
+function registration(){
+    return(
+      <h1>Go Fashion Registration</h1>
+    );
+  };
+  
+  export default registration;
