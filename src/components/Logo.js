0be944cedb4bbbf66a47efadb1941284ca766845
Luiz Guilherme da Costa <luizguilhermeh@yahoo.com.br>
2022-08-25 16:18:41 -0300

    Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logoheaders from "../img/logo.png";
+
+class Logo extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={logoheaders} alt="logo" />
+      </div>
+    );
+  }
+}
+
+export default Logo;
