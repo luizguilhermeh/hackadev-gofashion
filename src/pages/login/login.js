10da838fe453437d67985689a685b3ab7242fd16
Daniel Batista <oliveiradb1@gmail.com>
2022-08-25 20:27:22 -0300

    update login

--- src/pages/login/login.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function login(){
-  return(
-    <h1>Go Fashion Login</h1>
-  );
-};
-
-export default login;
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from "react";
+import { useState } from "react";
+
+import registration from "../registration/registration";
+import home from "../home/home"
+
+import icon from "./assets/favicon.ico";
+
+import "./login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title">Bem Vindo!</span>
+
+            {/* input para entrada de email e senha */}
+            <span className="login-form-title">
+              <img src={icon} alt="icon" />
+            </span>
+
+            <div className="wrap-input">
+              <input
+                className={email !== "" ? "has-val input" : "input"}
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input
+                className={password !== "" ? "has-val input" : "input"}
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+            {/* botão */}
+            <div className="container-login-form-button">
+              <button  className="login-form-button"><a href="home">Entrar</a></button>
+            </div>
+            {/* campo para redirecionamento criar conta */}
+            <div className="text-center">
+              <span className="texto1">Não possui conta?</span>
+
+              <a href="registration" className="texto2">
+                Criar Conta
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
