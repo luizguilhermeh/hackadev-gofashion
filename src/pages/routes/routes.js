de40cfdb0884acafa939444edb9ce9ddeee22641
luizguilhermeh <luizguilhermeh@yahoo.com.br>
2022-08-26 18:52:40 -0300

    routes

--- src/pages/routes/routes.js
+++ src/pages/routes/routes.js
@@ -12,7 +12,7 @@ function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={home} />
+        <Route path="/home" exact component={home} />
         <Route path="/product" component={product} />
         <Route path="/login" component={login} />
         <Route path="/checkout" component={checkout} />
