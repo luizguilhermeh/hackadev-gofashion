a952cd7309b96a80a846ef6f80e7e1de06e2dda6
Luiz Guilherme da Costa <luizguilhermeh@yahoo.com.br>
2022-08-26 11:23:20 -0300

    Icons

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,15 +1,15 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import "./Icons.css";
 
 library.add(fas);
 
 export const Icons = () => (
-  <div>
-    <FontAwesomeIcon icon="user" />
-    <FontAwesomeIcon icon="bag-shopping" />
+  <div className="icons">
+    <FontAwesomeIcon icon="user" id="icone-user"  />
+    <FontAwesomeIcon icon="bag-shopping" id="icone-bag"/>
     
   </div>
 );
-
 export default Icons;
