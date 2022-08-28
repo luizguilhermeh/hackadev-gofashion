05e9f1e18a379fc985a3f119253791c7bf28736c
luizguilhermeh <luizguilhermeh@yahoo.com.br>
2022-08-28 02:21:53 -0300

    Registration

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from './Registration.module.css'
+import Formulario from "../../components/Formulario";
 
 function registration() {
   return (
   <div className={styles.registration_container}>
     <h1>Crie seu cadastro</h1>
-    <p id="p-registro">*preenchimento obrigatório</p>
+    {/* <p id="p-registro">*preenchimento obrigatório</p> */}
+    <Formulario btnText="Cadastrar" />
   </div>
   );
 }
