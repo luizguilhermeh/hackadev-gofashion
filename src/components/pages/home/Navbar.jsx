4332f44bb0740c96ebb6871cbe9f48150572e4b6
luizguilhermeh <luizguilhermeh@yahoo.com.br>
2022-08-28 19:38:31 -0300

    Update

--- src/components/pages/home/Navbar.jsx
+++ src/components/pages/home/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import logo from "../../../img/logo.svg";
+import logo from "../../../img/logo.png";
 import shopping from "../../../img/shopping.svg";
-import '../home/home.css'
+import user from "../../../img/account.png";
+import "../home/home.css";
+import "../../Logo/Logo.css";
+import { Link } from "react-router-dom";
 
-import Cart from './Cart';
-import { useStateContext} from '../../../context/StateContext';
+import Cart from "./Cart";
+import { useStateContext } from "../../../context/StateContext";
 
 const Navbar = () => {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
@@ -13,12 +16,27 @@ const Navbar = () => {
     <>
       <div className="nav">
         <p>
-         <a href="/"> <img className="logo" src={logo} alt=''/></a>
+          <a href="/">
+            {" "}
+            <img className="logo" src={logo} alt="" />
+          </a>
         </p>
-        <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
-          <img src={shopping} id="ShoppingBagIcon" alt="imagem"/>
-           <span className="cart-item-qty">{totalQuantities}</span>
-        </button>
+        <div className="icons-nav">
+          <a href="/login">
+            <button type="button" className="cart-icon">
+              <img src={user} id="ShoppingBagIcon" alt="imagem" />
+            </button>
+          </a>
+
+          <button
+            type="button"
+            className="cart-icon"
+            onClick={() => setShowCart(true)}
+          >
+            <img src={shopping} id="ShoppingBagIcon" alt="imagem" />
+            <span className="cart-item-qty">{totalQuantities}</span>
+          </button>
+        </div>
         {showCart && <Cart />}
       </div>
     </>
