538f4d490772828b9fe67447d94ad46d741c7649
luizguilhermeh <luizguilhermeh@yahoo.com.br>
2022-08-28 02:21:44 -0300

    Submit Button

---
+++ src/components/form/SubmitButton.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import styles from "./SubmitButton.module.css";
+
+function SubmitButton({ text }) {
+  return (
+    <div>
+      <Link to="/home">
+        <button className={styles.btn}>{text}</button>
+        </Link>
+    </div>
+  );
+}
+export default SubmitButton;
