13e25aa0fa147a56bdecdf0cfaf7d61def67da3f
luizguilhermeh <luizguilhermeh@yahoo.com.br>
2022-08-28 13:34:34 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Routes from "./pages/routes/routes";
-import Headers from "./components/Headers";
-import Footer from "./components/Footer";
+import Routes from "./components/pages/routes/routes";
+import Headers from "./components/Headers/Headers";
+import Footer from "./components/Footer/Footer";
 import "./App.css"
 
 function App() {
