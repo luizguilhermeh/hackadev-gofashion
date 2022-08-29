5d761caac171ccf340e4ce5f3c172fcc4f207dcf
luizguilhermeh <luizguilhermeh@yahoo.com.br>
2022-08-29 19:04:51 -0300

    Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 
-function Footer(){
-  return(
-    <div className='footer'>
-      <h3 id='footer-title'>
-        Go Fashion
-      </h3>
-      <p id='footer-subtitle'>
-      Copyright©2022-2022,Go Fashion Company. Todos os direitos
-        reservados.
-      </p>
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-text">
+        <h3 id="footer-title">Go Fashion</h3>
+        <p id="footer-subtitle">
+          Copyright©2022-2022,Go Fashion Company. Todos os direitos reservados.
+        </p>
+      </div>
     </div>
   );
-};
+}
 
 export default Footer;
