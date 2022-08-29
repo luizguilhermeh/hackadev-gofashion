f76f417eb2bd77a3f80f91c46b290cbfadf41f85
Luiz Guilherme da Costa <luizguilhermeh@yahoo.com.br>
2022-08-29 14:29:05 -0300

    Formulario

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -9,9 +9,9 @@ function Formulario({btnText}){
     <form className={styles.form}>
         <Input type="text" text="Nome" name="name" placeholder="Nome Completo" />
         <Input type="text" text="E-mail" name="email" placeholder="email@dominio.com" />
-        <Input type="text" text="Data de Nascimento" name="birth" placeholder="___ / ___ / ___" />
-        <Input type="text" text="Telefone" name="phone" placeholder="(___) _______-______" />
-        <Input type="text" text="Celular" name="cellphone" placeholder="(___) _______-______" />
+        <Input type="text" text="Data de Nascimento" name="birth" placeholder="    /     /    " />
+        <Input type="text" text="Telefone" name="phone" placeholder="(    )"/>
+        <Input type="text" text="Celular" name="cellphone" placeholder="(    )" />
         <Input type="text" text="CPF" name="cpf" placeholder="000.000.000-00" />
         <Input type="text" text="CEP" name="cep" placeholder="74.000-000" />
         <Input type="text" text="Endereço" name="address" placeholder="Avenida, Rua, Alameda..." />
