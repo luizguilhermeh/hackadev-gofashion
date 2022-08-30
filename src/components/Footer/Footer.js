ceed99026adc463b8cc52aa7c6ef6079f43452db
aesh9 <ar50gyn13@gmail.com>
2022-08-30 01:12:51 -0300

    Checkout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,18 @@
-import React from "react";
-import "./Footer.css";
+import React from 'react';
+import "./Footer.css"
 
-function Footer() {
-  return (
-    <div className="footer">
-      <div className="footer-text">
-        <h3 id="footer-title">Go Fashion</h3>
-        <p id="footer-subtitle">
-          Copyright©2022-2022,Go Fashion Company. Todos os direitos reservados.
-        </p>
-      </div>
+function Footer(){
+  return(
+    <div className='footer'>
+      <h3 id='footer-title'>
+        Go Fashion
+      </h3>
+      <p id='footer-subtitle'>
+      Copyright©2022-2022,Go Fashion Company. Todos os direitos
+        reservados.
+      </p>
     </div>
   );
-}
+};
 
 export default Footer;
