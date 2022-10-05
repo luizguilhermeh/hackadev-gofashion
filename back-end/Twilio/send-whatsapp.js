6bf08981eb506fd108f1b052eb34268f618a28b0
Daniel Batista <oliveiradb1@gmail.com>
2022-10-05 15:54:33 -0300

    update twilio

--- back-end/Twilio/send-whatsapp.js
+++ back-end/Twilio/send-whatsapp.js
@@ -1,7 +1,7 @@
 
 
 const accountSid = 'AC577ee4893590d031dfc2502856f65d81'; // Your Account SID from www.twilio.com/console
-const authToken = '153d003c9051b0f25e08ce06628274f5'; // Your Auth Token from www.twilio.com/console
+const authToken = '0621619714ee99576ecef8fde795ebc3'; // Your Auth Token from www.twilio.com/console
 
 /// Acesso em: https://console.twilio.com/us1/develop/sms/try-it-out/whatsapp-learn?frameUrl=%2Fconsole%2Fsms%2Fwhatsapp%2Flearn%3Fx-target-region%3Dus1///
 
